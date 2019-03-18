0495fb1efbc063a124997aad66be56f251e131c9
Florent330 <florent3300047@gmail.com>
2019-03-18 12:33:00 +0100

    Create succes.php

---
+++ succes.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Formulaire</title>
+</head>
+<body>
+<div>
+    <p><?php echo 'Bravo !'?></p>
+</div>
+</body>
+</html>
